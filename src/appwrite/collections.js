2322bcf363762be56f306ee7b513c24d34738f1c
Husnain Ashfaq <127616463+Husnain-Ashfaq39@users.noreply.github.com>
2025-04-19 14:42:12 +0500

    Add CourseChat component to CourseDetailsPage and update collections with courseChats ID

--- src/appwrite/collections.js
+++ src/appwrite/collections.js
@@ -4,6 +4,7 @@ export const collections = {
     courses: '680341b7003a106fbc4a', // Replace with actual collection ID after creation
     courseContents: '68034a420017e656cda0', // Course content collection ID
     enrollments: '68035408001762c26cbd',
+    courseChats: '680367e90029802cf0ef', // This will need to be replaced with the actual collection ID after creation
 };
 
 export default collections;
